c1a3d100ba48a795de40e35d0d0561c82cf2b447
Egor Sorokin <egor.v.s@mail.ru>
2023-08-15 17:33:12 +0300

    GamePanel created

---
+++ src/main/java/GamePanel.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel {
+
+    // SCREEN SETTINGS
+    final int originalTileSize = 16; // начальный размер плитки для отображения объектов 16x16
+    final int scale = 3;
+
+    final int tileSize = originalTileSize * scale; // итоговый размер плитки 48x48
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
+    final int screenWidth = tileSize * maxScreenCol; // ширирна 768 пикселей
+    final int screenHeight = tileSize * maxScreenRow; // высота 576 пикселей
+
+    public GamePanel() {
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight)); // размеры окна
+        this.setBackground(Color.black); // цвет фона
+        this.setDoubleBuffered(true); // улучшенный рендеринг
+    }
+}
