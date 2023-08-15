44490b805e7c7534ded1870fc7cec534113d7222
Egor Sorokin <egor.v.s@mail.ru>
2023-08-15 17:51:16 +0300

    window generator updated

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,18 @@
+import javax.swing.*;
+
 public class Main {
-  public static void main(String[] args) {
+    public static void main(String[] args) {
         JFrame window = new JFrame(); // создание окна
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // закрытие окна по нажатию на кнопку "X"
         window.setResizable(false); // фиксирование размера окна
         window.setTitle("Game"); // название окна
 
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel); // добавление параметров экрана
+
+        window.pack(); // установка оптимального размера для окна
+
         window.setLocationRelativeTo(null); // отображение окна по центру
         window.setVisible(true); // установка отображения окна
-    }
+  }
 }
