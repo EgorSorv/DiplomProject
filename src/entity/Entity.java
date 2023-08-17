02c358a2e6d64bd1a27df37bcf4f9ffc628b3676
Egor Sorokin <egor.v.s@mail.ru>
2023-08-17 11:18:03 +0300

    getPlayerImage added in Player

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -1,5 +1,9 @@
 package entity;
 
+import java.awt.image.BufferedImage;
+
 public class Entity {
     public int x, y, speed;
+    public BufferedImage upIdle, up1, up2, downIdle, down1, down2,
+            leftIdle, left1, left2, rightIdle, right1, right2;
 }
--- src/entity/Player.java
+++ src/entity/Player.java
@@ -3,7 +3,10 @@ package entity;
 import main.GamePanel;
 import main.KeyHandler;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.io.IOException;
+import java.util.Objects;
 
 public class Player extends Entity {
     GamePanel gamePanel;
@@ -14,12 +17,56 @@ public class Player extends Entity {
         this.keyHandler = keyHandler;
 
         setDefaultValues();
+        getPlayerImage();
     }
 
     public void setDefaultValues() {
         x = 100;
         y = 100;
         speed = 4;
+    }
+
+    // получение изображения
+    public void getPlayerImage() {
+        try {
+            upIdle = ImageIO.read(Objects.requireNonNull(
+                    getClass().getResourceAsStream("/player/Player_up_idle.png")));
+
+            up1 = ImageIO.read(Objects.requireNonNull(
+                    getClass().getResourceAsStream("/player/Player_up_1.png")));
+
+            up2 = ImageIO.read(Objects.requireNonNull(
+                    getClass().getResourceAsStream("/player/Player_up_2.png")));
+
+            downIdle = ImageIO.read(Objects.requireNonNull(
+                    getClass().getResourceAsStream("/player/Player_down_idle.png")));
+
+            down1 = ImageIO.read(Objects.requireNonNull(
+                    getClass().getResourceAsStream("/player/Player_down_1.png")));
+
+            down2 = ImageIO.read(Objects.requireNonNull(
+                    getClass().getResourceAsStream("/player/Player_down_2.png")));
+
+            leftIdle = ImageIO.read(Objects.requireNonNull(
+                    getClass().getResourceAsStream("/player/Player_left_idle.png")));
+
+            left1 = ImageIO.read(Objects.requireNonNull(
+                    getClass().getResourceAsStream("/player/Player_left_1.png")));
+
+            left2 = ImageIO.read(Objects.requireNonNull(
+                    getClass().getResourceAsStream("/player/Player_left_2.png")));
+
+            rightIdle = ImageIO.read(Objects.requireNonNull(
+                    getClass().getResourceAsStream("/player/Player_right_idle.png")));
+
+            right1 = ImageIO.read(Objects.requireNonNull(
+                    getClass().getResourceAsStream("/player/Player_right_1.png")));
+
+            right2 = ImageIO.read(Objects.requireNonNull(
+                    getClass().getResourceAsStream("/player/Player_right_2.png")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void update() {
