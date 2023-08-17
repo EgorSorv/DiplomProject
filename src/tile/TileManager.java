8c778fc77a9d5be15b58de958ef3294ac03bcf7d
Egor Sorokin <egor.v.s@mail.ru>
2023-08-17 16:08:23 +0300

    getTileImage added in TileManager

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -2,6 +2,10 @@ package tile;
 
 import main.GamePanel;
 
+import javax.imageio.ImageIO;
+import java.io.IOException;
+import java.util.Objects;
+
 public class TileManager {
     GamePanel gamePanel;
     Tile[] tiles; // массив для хранения плиток
@@ -15,6 +19,20 @@ public class TileManager {
     }
 
     public void getTileImage() {
+        try {
+            tiles[0] = new Tile();
+            tiles[0].image = ImageIO.read(Objects.requireNonNull(
+                    getClass().getResourceAsStream("/tile/grass.png")));
 
+            tiles[1] = new Tile();
+            tiles[1].image = ImageIO.read(Objects.requireNonNull(
+                    getClass().getResourceAsStream("/tile/wall.png")));
+
+            tiles[2] = new Tile();
+            tiles[2].image = ImageIO.read(Objects.requireNonNull(
+                    getClass().getResourceAsStream("/tile/water.png")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
