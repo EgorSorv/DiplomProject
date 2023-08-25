be481cacba03e5e2ca0798c6c79c8daa3c132db3
Egor Sorokin <egor.v.s@mail.ru>
2023-08-25 13:22:40 +0300

    new object addeded in AssetSetter

--- src/main/AssetSetter.java
+++ src/main/AssetSetter.java
@@ -1,5 +1,7 @@
 package main;
 
+import object.Chest;
+import object.Door;
 import object.Key;
 
 public class AssetSetter {
@@ -18,5 +20,25 @@ public class AssetSetter {
         gamePanel.obj[1] = new Key();
         gamePanel.obj[1].worldX = 23 * gamePanel.tileSize;
         gamePanel.obj[1].worldY = 40 * gamePanel.tileSize;
+
+        gamePanel.obj[2] = new Key();
+        gamePanel.obj[2].worldX = 38 * gamePanel.tileSize;
+        gamePanel.obj[2].worldY = 8 * gamePanel.tileSize;
+
+        gamePanel.obj[3] = new Door();
+        gamePanel.obj[3].worldX = 10 * gamePanel.tileSize;
+        gamePanel.obj[3].worldY = 11 * gamePanel.tileSize;
+
+        gamePanel.obj[4] = new Door();
+        gamePanel.obj[4].worldX = 8 * gamePanel.tileSize;
+        gamePanel.obj[4].worldY = 28 * gamePanel.tileSize;
+
+        gamePanel.obj[5] = new Door();
+        gamePanel.obj[5].worldX = 12 * gamePanel.tileSize;
+        gamePanel.obj[5].worldY = 22 * gamePanel.tileSize;
+
+        gamePanel.obj[6] = new Chest();
+        gamePanel.obj[6].worldX = 10 * gamePanel.tileSize;
+        gamePanel.obj[6].worldY = 7 * gamePanel.tileSize;
     }
 }
