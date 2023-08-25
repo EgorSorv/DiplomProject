78205becfe486e16b7b4cc8574989420749dba1d
Egor Sorokin <egor.v.s@mail.ru>
2023-08-25 23:42:59 +0300

    scaleImage added in UtilityTool

--- src/main/UtilityTool.java
+++ src/main/UtilityTool.java
@@ -1,5 +1,17 @@
 package main;
 
-public class UtilityTool {
+import java.awt.*;
+import java.awt.image.BufferedImage;
 
+public class UtilityTool {
+    // масштабирование изображения
+    public BufferedImage scaleImage(BufferedImage original, int width, int height) {
+        BufferedImage scaledImage = new BufferedImage(width, height, original.getType());
+
+        Graphics2D graphics2D = scaledImage.createGraphics();
+        graphics2D.drawImage(original, 0, 0, width, height, null);
+        graphics2D.dispose();
+
+        return scaledImage;
+    }
 }
