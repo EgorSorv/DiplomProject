e149bb06d1260f0fd534649208851e390299bdad
Egor Sorokin <egor.v.s@mail.ru>
2023-08-25 15:05:49 +0300

    Sound class created

---
+++ src/main/Sound.java
@@ -0,0 +1,19 @@
+package main;
+
+import javax.sound.sampled.Clip;
+import java.net.URL;
+
+public class Sound {
+    Clip clip;
+    URL[] soundURL = new URL[30];
+
+    public Sound() {
+        soundURL[0] = getClass().getResource("/sounds/Game.wav");
+        soundURL[1] = getClass().getResource("/sounds/coin.wav");
+        soundURL[2] = getClass().getResource("/sounds/powerup.wav");
+        soundURL[3] = getClass().getResource("/sounds/unlock.wav");
+        soundURL[4] = getClass().getResource("/sounds/fanfare.wav");
+    }
+
+
+}
